651dd76e4868422ad0dba14249975d2e3cba54a0
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-04 12:01:46 +0530

    seting up a db connection wraper utils api error handeler in node and response handeler

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@
 import dotenv from "dotenv";
 
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is runing at Port ${process.env.PORT}`);
+    });
+  })
+  .catch((e) => console.log("MONGO DB CONNECTION FAILED", e));
