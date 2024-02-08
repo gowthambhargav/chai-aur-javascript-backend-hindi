ba76f18e8796eab22159c9901fb7a03ba0de453f
gowthambhargav <gowthambhargav26@gmail.com>
2024-02-08 16:59:22 +0530

    add routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChannelProfile,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccountDetails,
+  updateUserAvatar,
+  updateUserCoverImage,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verfyJWT } from "../middlewares/auth.middleware.js";
@@ -29,5 +36,18 @@ router.route("/login").post(loginUser);
 // secured routes
 router.route("/logout").post(verfyJWT, logoutUser);
 router.route("/refresh-token").post(refreshAccessToken); // if refreshAccessToken controler is writen in the different file , verfyJWT middleware is required
+router.route("/change-password").post(verfyJWT, changeCurrentPassword);
+router.route("/current-user").get(verfyJWT, getCurrentUser);
+router.route("/update-account").patch(verfyJWT, updateAccountDetails);
+
+router
+  .route("/avatar")
+  .patch(verfyJWT, upload.single("avatar"), updateUserAvatar);
+router
+  .route("/cover-image")
+  .patch(verfyJWT, upload.single("coverImage"), updateUserCoverImage);
+
+router.route("/c/:username").get(verfyJWT, getUserChannelProfile);
+router.route("/history").get(verfyJWT, getWatchHistory);
 
 export default router;
