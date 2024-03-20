f96b08de105ba626382d385c4be939462b83943e
gowthambhargav <gowthambhargav26@gmail.com>
2024-03-20 20:04:17 +0530

    Commented out import of swaggerDoc in index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 // require('dotenv').config({path: './env'})
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-import swaggerDoc from "./swagger.js";
+// import swaggerDoc from "./swagger.js";
 
 import {app} from './app.js'
 dotenv.config({
